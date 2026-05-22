55c454bae4f9eadd02c595ae2f03126db91ce3f2
Caio Ronald Silva <l4ss4r0t3@gmail.com>
2026-05-21 22:40:43 -0300

    testing functions in c

---
+++ LINGUAGEM DE PROGRAMAÇÃO I/TESTES/PRAT. DE FUNÇÕES.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a, b, c;
+    
+    printf("Digite o Valor para a:");
+    scanf("%d", a)
+    printf("Digite o Valor para b:");
+    scanf("%d", b)
+    
+    printf("Resultado de a + b: "%d, a + b)
+
+    return 0;
+    
+}
